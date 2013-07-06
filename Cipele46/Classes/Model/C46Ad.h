3ae4b2645cc847f9791b5b0718c91abf3f9c8ea2
Ivana Rast <ivana.rast@gmail.com>
2013-07-06 13:04:48 +0200

    Added properties to C46Ad.h

--- Cipele46/Classes/Model/C46Ad.h
+++ Cipele46/Classes/Model/C46Ad.h
@@ -10,4 +10,20 @@
 
 @interface C46Ad : NSObject
 
+@property (strong, nonatomic) NSString *status;
+
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *imageURL;
+@property (strong, nonatomic) NSString *description;
+
+@property (strong, nonatomic) NSString *district;
+@property (strong, nonatomic) NSString *city;
+@property (strong, nonatomic) NSString *address;
+
+@property (strong, nonatomic) NSString *mail;
+@property (strong, nonatomic) NSString *phone;
+
+@property (nonatomic, getter=isApproved) BOOL approved;
+@property (nonatomic, getter=isFavourite) BOOL favourite;
+
 @end
