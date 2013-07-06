7ad121af28c22803f4136a20cf33b116e911fca1
Zoran Plesko <zoran.plesko@gmail.com>
2013-07-06 13:43:26 +0200

    ListAdView

--- Cipele46/Classes/Model/C46Ad.h
+++ Cipele46/Classes/Model/C46Ad.h
@@ -10,4 +10,16 @@
 
 @interface C46Ad : NSObject
 
+@property (nonatomic, retain) NSNumber *status;
+@property (nonatomic, retain) NSString *description;
+@property (nonatomic, retain) NSNumber *districtID;
+@property (nonatomic, retain) NSString *phone;
+@property (nonatomic, retain) NSNumber *categoryID;
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *imageURL;
+@property (nonatomic, retain) NSNumber *adID;
+@property (nonatomic, retain) NSString *cityID;
+@property (nonatomic, retain) NSNumber *type;
+@property (nonatomic, retain) NSString *email;
+
 @end
