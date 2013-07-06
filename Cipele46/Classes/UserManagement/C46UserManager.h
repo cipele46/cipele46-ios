622fd8c4b4b837ca3b8b46ec3c3bc71f34225192
Drago Ruzman <drago.ruzman@gmail.com>
2013-07-06 12:59:30 +0200

    Adding user management code

---
+++ Cipele46/Classes/UserManagement/C46UserManager.h
@@ -0,0 +1,13 @@
+//
+//  C46UserManager.h
+//  Cipele46
+//
+//  Created by Drago on 7/6/13.
+//  Copyright (c) 2013 Miran Brajsa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface C46UserManager : NSObject
+
+@end
---
+++ Cipele46/Classes/UserManagement/C46UserManager.m
@@ -0,0 +1,13 @@
+//
+//  C46UserManager.m
+//  Cipele46
+//
+//  Created by Drago on 7/6/13.
+//  Copyright (c) 2013 Miran Brajsa. All rights reserved.
+//
+
+#import "C46UserManager.h"
+
+@implementation C46UserManager
+
+@end
