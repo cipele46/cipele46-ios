df9ee6fa109798e13d5c1ec20532e5e294abc499
Melvin <Nuklearko@Melvins-Mac.local>
2013-07-06 12:10:15 +0200

    Added and implemented partialy LoginViewController

---
+++ Cipele46/Classes/LoginViewController/LoginViewController.h
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.h
+//  Cipele46
+//
+//  Created by Melvin on 7/6/13.
+//  Copyright (c) 2013 Miran Brajsa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController <UITextFieldDelegate>
+
+#pragma mark - Properties
+
+@property (retain, nonatomic) IBOutlet UITextField* fieldEmail;
+@property (retain, nonatomic) IBOutlet UITextField* fieldPassword;
+
+#pragma mark - Button events
+
+-(IBAction)tryFacebookLogin:(id)sender;
+-(IBAction)tryNormalLogin:(id)sender;
+-(IBAction)forgotPassword:(id)sender;
+-(IBAction)registerUser:(id)sender;
+
+@end
